e62e840368d1443fd9f661ec5f1f445b1eed1370
kevindesai777 <kevindesai777@gmail.com>
2015-05-15 22:19:50 +0530

    Code formatting

--- app/src/main/java/moments/app/com/MainActivity.java
+++ app/src/main/java/moments/app/com/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
             FragmentInvite fragmentInvite = new FragmentInvite();
             fragmentTransaction.add(R.id.container, fragmentInvite);
             fragmentTransaction.commit();
-
         }
         else
         {
@@ -51,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R                                                                  .menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
