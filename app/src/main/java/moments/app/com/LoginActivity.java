3d9f37ada82055a06080ec56436bc8a9ffe9f9cd
kevindesai777 <kevindesai777@gmail.com>
2015-05-21 21:35:00 +0530

    Custom font and layout optimized

--- app/src/main/java/moments/app/com/LoginActivity.java
+++ app/src/main/java/moments/app/com/LoginActivity.java
@@ -1,10 +1,10 @@
 package moments.app.com;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.*;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -12,7 +12,14 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
     }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
 
 }
